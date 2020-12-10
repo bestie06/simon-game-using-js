1c0f862903552293f31b8cac47141e4a64f07b00
bestie06 <fatih.kgokmen@gmail.com>
2020-12-10 13:46:51 -0500

    game.js
    created game js

---
+++ game.js
@@ -0,0 +1,9 @@
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern=[];
+
+function nextSequence(){
+   var randomNumber = Math.floor(Math.random()*4);
+   var randomChosenColor = buttonColours[randomNumber];
+   gamePattern.push(randomChosenColor);
+}
