903943a7e88b79691f9d0a00a5c1fe9e6bd5dd8a
bestie06 <fatih.kgokmen@gmail.com>
2020-12-10 16:25:48 -0500

    updated
    updated

--- game.js
+++ game.js
@@ -1,9 +1,14 @@
 
-var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern=[];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-function nextSequence(){
-   var randomNumber = Math.floor(Math.random()*4);
-   var randomChosenColor = buttonColours[randomNumber];
-   gamePattern.push(randomChosenColor);
+var gamePattern = [];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
